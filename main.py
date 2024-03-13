401813388760794126a109cb4ac0ba54b52c8d76
Edecy-Gomes <124404460+edcgomes@users.noreply.github.com>
2024-03-13 12:10:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+#Desafio Estágio Ribeirão Preto
+#1)
+indice = 13
+k = 0
+soma = 0
+
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+print(soma)
+
+#2)
+number = int(input("insert the number you want to know if it's fibonacci: "))
+
+a = 0
+b = 1
+fibo = [a, b]
+
+while a < number:
+    c = a + b
+    a = b
+    b = c
+    fibo.append(c)
+
+print(fibo)
+if number in fibo:
+    print(f"{number} é um numero da sequencia fibonacci")
+else:
+    print(f"{number} não é um numero da sequencia fibonacci")
+
+#5)
+
+palavra = input("insira a palavra que deseja inverter: ")
+
+lista_palavra = list(palavra)
+x = len(lista_palavra)
+palavra_invertida = []
+
+for i in range(x):
+    a = (i + 1) * - 1
+    letra = lista_palavra[a]
+    palavra_invertida.append(letra)
+
+
+print(palavra_invertida)
+palavra = "".join(palavra_invertida)
+print(palavra)
+
